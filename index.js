df7d52ead6c89be8b60fb9c9d6e7d1c43fa4c188
Priyanka Garg <priyaanka.garg@gmail.com>
2018-10-19 19:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+
+
+
+theBeatlesPlay(musicians, instruments); 
+
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = []; 
+  for(var i =0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArr;
+}
+
+johnLennonFacts(facts) {
+  var i = facts.length;
+  while (i>=0) {
+    facts[i]+="!!!"
+  }
+  return facts;
+  
+}
